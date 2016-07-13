c54ac377c86f1d227d72afda7b06f46fa01f6263
Malavika Iyer <Malavika@Malavikas-MacBook-Pro.local>
2016-07-12 20:19:30 -0700

    First commit

---
+++ questions.js
@@ -0,0 +1,116 @@
+var ques=[[
+    {
+        question: "Where is the great Barrier Reef?",
+        questionType: 1,
+        choices: ["Africa", "North America", "Asia", "Australia"],
+        correctChoice: "Australia",
+        score:5,
+        hint: "It has kangaroos!"
+    },
+    {
+        question: "What planet is the Curiosity Rover on now?",
+        questionType: 2,
+        correctAnswer: "Mars",
+        score:10,
+        hint: "The planet is near earth."
+    },
+    {
+        question: "What is the highest mountain on Earth?",
+        questionType: 2,
+        correctAnswer: "Mount Everest",
+        score:10,
+        hint: "The peak is above 8000m."
+    },
+    {
+        question: "Where are lines of longitude farthest apart?",
+        questionType:1,
+        choices:["At the Prime Meridian", "At the Equator", "At the Date Line","At the South Pole"],
+        correctChoice:"At the Equator",
+        score:5,
+        hint: "It's in the middle"
+    },
+    {
+        question: "What is the capital of India?",
+        questionType:1,
+        choices:[ "Mumbai", "Beijing", "London","New Delhi"],
+        correctChoice:"New Delhi",
+        score:5,
+        hint: "It's in the North"
+    }],
+    [{
+        question: "Who was the first President of the United States?",
+        questionType:1,
+        choices:["John Adams", "George Washington", "Andrew Jackson", "Thomas Jefferson"],
+        correctChoice:"George Washington",
+        score:5,
+        hint: "He's on the $1 bill."
+    },
+    {
+        question: "Today in the United States government there are two major political parties.  The Republicians are one of the parties who is the other party?",
+        questionType: 2,
+        correctAnswer: "Democrats",
+        score:10,
+        hint: "Starts with D"
+    },
+    {
+        question: "Adolf Hitler was born in which country?",
+        questionType: 2,
+        correctAnswer: "Austria",
+        score:10,
+        hint: "The country is in Europe"
+    },
+    {
+        question: "In what year did America land the first man on the moon?",
+        questionType:1,
+        choices:["1969", "1965", "1968", "1970"],
+        correctChoice: "1969",
+        score:5,
+        hint: "Its in the 1960s"
+    },
+    {
+        question: "World War 2 began in which year?",
+        questionType:1,
+        choices: ["1917", "1923", "1939","1912"],
+        correctChoice: "1939",
+        score:5,
+        hint: "Later than you think"
+    }],
+    [{
+        question: "What kind of waves are used to make and receive cellphone call?",
+        questionType:1,
+        choices:["Radio waves", "Visible light waves", "Sound waves", "Gravity waves"],
+        correctChoice:"Radio waves",
+        score:5,
+        hint: "Its in the air"
+    },
+    {
+        question: "What does a light-year measure?",
+        questionType: 2,
+        correctAnswer: "Distance",
+        score:10,
+        hint: "Its not time"
+    },
+    {
+        question: "What element is needed to make nuclear energy and nuclear weapons?",
+        questionType: 2,
+        correctAnswer: "Uranium",
+        score:10,
+        hint: "Starts with U"
+    },
+    {
+        question: "Which is the main way that ocean tides are created?",
+        questionType:1,
+        choices:["The rotation of the Earth on its axis", "The gravitational pull of the Moon", "The gravitational pull of the Sun","All of the above"],
+        correctChoice: "The gravitational pull of the Moon",
+        score:5, 
+        hint: "Not the planet we live in"
+    },
+    {
+        question: "Which of these people developed the polio vaccine?",
+        questionType:1,
+        choices: ["Marie Curie", "Isaac Newton", "Albert Einstein", "Jonas Salk"],
+        correctChoice: "Jonas Salk",
+        score:5,
+        hint: "He's an American"
+    }]];
+
